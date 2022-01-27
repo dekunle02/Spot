d0e0e8e02e71cedd9a698ba583e669f64ac9cc19
dekunle02 <dekunle02@gmail.com>
2022-01-27 02:52:39 +0000

    updated ignore

--- Spot/controller/tele.py
+++ Spot/controller/tele.py
@@ -4,19 +4,16 @@ from dotenv import load_dotenv
 from telegram import Bot,Update, File
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
+from database import fire
 from . import replies
 
 TELEGRAM_TOKEN = os.environ['TELEGRAM_TOKEN']
 
-class State:
-    IDLE = 'idle'
-    SETUP = 'setup'
-    TIMESHEET = 'timesheet'
-    
 
 def help_command_handler(update: Update, context: CallbackContext):
     update.message.reply_text(text=replies.HELP_MESSAGE)
 
 
 def setup_command_handler(update: Update, context: CallbackContext):
+    
     pass
